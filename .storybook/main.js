d4e9fdc811702977d20f1e2125d63d7029823374
vivalafreak1 <arieftaufikrahman123@gmail.com>
2024-04-28 13:17:43 +0700

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,22 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+    "@storybook/addon-docs",
+  ],
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: true,
+  },
+  staticDirs: ["..\\public"],
+};
+export default config;
